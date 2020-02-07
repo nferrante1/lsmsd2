e24acdb2be42057f04ba3ddaf7da9cb564d13c45
spampas <49267419+spampas@users.noreply.github.com>
2020-02-07 10:48:32 +0100

    Update module-info.java

--- Scraper/src/module-info.java
+++ Scraper/src/module-info.java
@@ -1,12 +1,13 @@
 module scraper
 {
-	requires org.mongodb.driver.sync.client;
-	requires org.mongodb.bson;
-	requires org.mongodb.driver.core;
-	requires com.google.gson;
-	requires retrofit2;
-	requires retrofit2.converter.gson;
+
 	
 	exports app.scraper.data to com.google.gson;
 	opens app.scraper.data to com.google.gson;
+	requires org.mongodb.bson;
+	requires com.google.gson;
+	requires org.mongodb.driver.core;
+	requires org.mongodb.driver.sync.client;
+	requires retrofit2;
+	requires retrofit2.converter.gson;
 }
