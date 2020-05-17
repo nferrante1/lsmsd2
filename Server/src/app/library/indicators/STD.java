a62d865e6a0e4666bd022c6b7bf2f2ef6e1554bc
Nicola Ferrante <nicolaferrante96@gmail.com>
2020-05-17 19:36:31 +0200

    Added MOM EMA and STD indicators

---
+++ Server/src/app/library/indicators/STD.java
@@ -0,0 +1,119 @@
+package app.library.indicators;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.bson.BsonNull;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import com.mongodb.client.model.Accumulators;
+import com.mongodb.client.model.Aggregates;
+import com.mongodb.client.model.Field;
+import com.mongodb.client.model.Projections;
+
+import app.library.Candle;
+import app.library.indicators.enums.InputPrice;
+
+public class STD extends Indicator implements ComputableIndicator {
+	private int period;
+	private InputPrice inputPrice;
+	private double value = Double.NaN;
+	private long elapsedPeriods;
+	
+	@Override
+	public String getName()
+	{
+		return "STD" + inputPrice.getShortName() + period;
+	}
+
+	@Override
+	public List<Bson> getPipeline()
+	{
+		List<Bson> projections = new ArrayList<Bson>();
+		Document push = new Document("c", "$c");
+		Document mapDoc = null;
+		projections.add(Projections.excludeId());
+	
+		switch(inputPrice) {
+			case CLOSE:
+				projections.add(Projections.computed("v" , "$c"));
+				break;
+			case INCREMENT:
+				projections.add(Projections.include("c","o"));
+				push.append("o", "$o");
+				mapDoc = new Document("$max", Arrays.asList(
+						new Document("$subtract",
+								Arrays.asList("$$candle.c", "$$candle.o")), 0L));
+				break;
+			case DECREMENT:
+				projections.add(Projections.include("c","o"));
+				push.append("o", "$o");
+				mapDoc = new Document("$max", Arrays.asList(
+						new Document("$subtract",
+								Arrays.asList("$$candle.o", "$$candle.c")), 0L));
+				break;
+			case TRUE_RANGE:
+				projections.add(Projections.include("h","l","c"));
+				push.append("l", "$l");
+				push.append("h", "$h");
+				mapDoc = new Document("$max", Arrays.asList(new Document("$subtract", 
+						Arrays.asList("$h","$l")), 
+						new Document("$abs", 
+							new Document("$subtract",
+									Arrays.asList("$h", "$c")
+								)
+							)
+						, new Document("$abs", 
+							new Document("$subtract", 
+								Arrays.asList("$l", "$c")
+								)
+							)
+						)
+					);
+				break;
+			case TYPICAL:
+				projections.add(Projections.include("h","l","c"));
+				push.append("l", "$l");
+				push.append("h", "$h");
+				mapDoc = new Document("$divide", Arrays.asList(new Document("$sum", Arrays.asList("$h","$l","$c")),3));
+				break;			
+		}
+		
+		List<Bson> stages = new ArrayList<Bson>();
+		stages.add(Aggregates.project(Projections.fields(projections)));
+		stages.add(Aggregates.group(new BsonNull(),Accumulators.push("candles", push)));
+		if(inputPrice != InputPrice.CLOSE)
+			stages.add(Aggregates.addFields(new Field<Document>("candles", new Document("$map",
+					new Document("input", "$candles")
+					.append("as", "candle")
+					.append("in", new Document("v", mapDoc))))));
+		stages.add(Aggregates.addFields(new Field<Document>("candles",						
+				new Document("$map",
+					new Document("input", new Document("$range", Arrays.asList(0L, new Document("$subtract", Arrays.asList(new Document("$size", "$candles"), 1L)))))
+				.append("as", "z")
+				.append("in",
+					new Document("value", new Document("$stdDevPop", new Document("$slice", Arrays.asList("$candles", 
+				                            new Document("$max", Arrays.asList(0L, 
+					                                    new Document("$subtract", Arrays.asList("$$z", period)))), period))
+							))
+					)
+				))));
+		return stages;
+	}
+
+	@Override
+	public void compute(Candle candle)
+	{	
+		++elapsedPeriods;
+		if(elapsedPeriods >= period)
+			value = candle.getTa(getName());
+	}
+
+	public double getValue()
+	{
+		return value;
+	}
+
+}
