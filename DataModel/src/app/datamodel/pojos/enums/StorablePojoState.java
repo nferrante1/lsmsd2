c094cfff8f91dc2997fd779c24f9826b7beb4e8b
Niccolò Scatena <speedjack95@gmail.com>
2020-06-07 18:54:27 +0200

    Add comments to POJO states

--- DataModel/src/app/datamodel/pojos/enums/StorablePojoState.java
+++ DataModel/src/app/datamodel/pojos/enums/StorablePojoState.java
@@ -2,9 +2,9 @@ package app.datamodel.pojos.enums;
 
 public enum StorablePojoState
 {
-	INIT,
-	IGNORED,
-	UNTRACKED,
-	STAGED,
-	COMMITTED
+	INIT,		/* The MongoDB driver has created the POJO class and it is filling its fields */
+	IGNORED,	/* Ignored (detached) POJO. Modifications will not be registered and saved */
+	UNTRACKED,	/* Newly created POJO, still not saved */
+	STAGED,		/* POJO contains some unsaved modifications */
+	COMMITTED	/* POJO is up-to-date with the document in the db */
 }
